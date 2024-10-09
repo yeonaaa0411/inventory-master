5f9a67e49591006b2bd89f1e6fa872fdf303a30f
Marvin Cruz <marvinkristianmcruz2@gmail.com>
2024-10-09 08:57:19 +0800

    Update stock.php

--- stock.php
+++ stock.php
@@ -1,93 +1,119 @@
 <?php
-$page_title = 'All stock';
+$page_title = 'All Stock';
 require_once('includes/load.php');
-// Checkin What level user has permission to view this page
+// Check what level user has permission to view this page
 page_require_level(2);
 
 $all_stock = find_all('stock');
 $all_products = find_all('products');
 ?>
 
-<!--     *************************     -->
+<!DOCTYPE html>
+<html lang="en">
 
-<?php include_once('layouts/header.php'); ?>
-<div class="row">
-    <div class="col-md-12">
-        <?php echo display_msg($msg); ?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo isset($page_title) ? remove_junk($page_title) : "Admin"; ?></title>
+
+    <!-- Tailwind CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+    <style>
+        th,
+        td {
+            padding: 25px;
+            border: 1px solid #e2e8f0; /* Change border to 1px for better visibility */
+        }
+
+        th {
+            background-color: #eaf5e9;
+        }
+
+        table {
+            border-collapse: collapse; /* Ensure borders collapse */
+            width: 100%; /* Ensure table takes full width */
+            
+        }
+
+        tr:hover {
+            background-color: #f7fafc; /* Light hover effect for rows */
+        }
+    </style>
+</head>
+
+<body class="bg-gray-100">
+    <?php include_once('layouts/header.php'); ?>
+    
+    <div class="flex justify-center">
+        <div class="w-11/12 md:w-2/3">
+            <?php echo display_msg($msg); ?>
+        </div>
     </div>
-</div>
 
-<div class="row">
-    <div class="col-md-12">
-        <div class="panel panel-default">
-            <div class="panel-heading clearfix">
-                <strong>
-                    <span class="glyphicon glyphicon-th"></span>
-                    <span>Inventory Log</span>
+    <div class="grid grid-cols-1 mt-6 mx-5">
+        <div class="bg-white shadow-md rounded-lg">
+            <div class="flex justify-between items-center p-4 border-b">
+                <strong class="text-3xl font-bold">
+                    <i class="fas fa-box mr-2"></i>
+                    Inventory Log
                 </strong>
                 <div class="pull-right">
-                    <a href="add_stock.php" class="btn btn-primary">Add Stock</a>
+                    <a href="add_stock.php" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Add Stock</a>
                 </div>
             </div>
-            <div class="panel-body">
-                <!--     *************************     -->
-                <table class="table table-bordered table-striped table-hover">
+            <div class="p-4">
+                <table class="min-w-full border-collapse">
                     <thead>
                         <tr>
-                            <th class="text-center" style="width: 50px;">#</th>
-                            <th class="text-center" style="width: 50px;">Product</th>
-                            <th class="text-center" style="width: 15%;">Quantity</th>
-                            <th class="text-center" style="width: 50px;">Comments</th>
-                            <th class="text-center" style="width: 15%;">Date</th>
-                            <th class="text-center" style="width: 100px;">Actions</th>
+                            <th class="text-center border px-4 py-2" style="width: 50px;">#</th>
+                            <th class="text-center border px-4 py-2" style="width: 15%;">Product</th>
+                            <th class="text-center border px-4 py-2" style="width: 15%;">Quantity</th>
+                            <th class="text-center border px-4 py-2" style="width: 50px;">Comments</th>
+                            <th class="text-center border px-4 py-2" style="width: 15%;">Date</th>
+                            <th class="text-center border px-4 py-2" style="width: 100px;">Actions</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <!--     *************************     -->
                         <?php foreach ($all_stock as $index => $stock): ?>
-                        <tr>
-                            <td class="text-center"><?php echo $index + 1; ?></td>
-                            <td class="text-center">
-                                <a href="view_product.php?id=<?php echo (int)$stock['product_id']; ?>">
-                                    <?php
-                                    foreach ($all_products as $product) {
-                                        if ($stock['product_id'] == $product['id']) {
-                                            echo remove_junk($product['name']);
+                            <tr>
+                                <td class="text-center"><?php echo $index + 1; ?></td>
+                                <td class="text-center">
+                                    <a href="view_product.php?id=<?php echo (int)$stock['product_id']; ?>">
+                                        <?php
+                                        foreach ($all_products as $product) {
+                                            if ($stock['product_id'] == $product['id']) {
+                                                echo remove_junk($product['name']);
+                                            }
                                         }
-                                    }
-                                    ?>
-                                </a>
-                            </td>
-                            <td class="text-center"><?php echo remove_junk(ucfirst($stock['quantity'])); ?></td>
-                            <td class="text-center"><?php echo remove_junk(ucfirst($stock['comments'])); ?></td>
-                            <td class="text-center"><?php echo remove_junk(ucfirst($stock['date'])); ?></td>
-                            <td class="text-center">
-                                <div class="btn-group">
-                                    <a href="edit_stock.php?id=<?php echo (int)$stock['id']; ?>" class="btn btn-warning btn-xs" data-toggle="tooltip" title="Edit">
-                                        <span class="glyphicon glyphicon-edit"></span>
+                                        ?>
                                     </a>
-                                    <a href="delete_stock.php?id=<?php echo (int)$stock['id']; ?>" onClick="return confirm('Are you sure you want to delete?')" class="btn btn-danger btn-xs" data-toggle="tooltip" title="Remove">
-                                        <span class="glyphicon glyphicon-trash"></span>
-                                    </a>
-                                </div>
-                            </td>
-                        </tr>
+                                </td>
+                                <td class="text-center"><?php echo remove_junk(ucfirst($stock['quantity'])); ?></td>
+                                <td class="text-center"><?php echo remove_junk(ucfirst($stock['comments'])); ?></td>
+                                <td class="text-center"><?php echo remove_junk(ucfirst($stock['date'])); ?></td>
+                                <td class="text-center">
+                                    <div class="flex justify-center space-x-2">
+                                        <a href="edit_stock.php?id=<?php echo (int)$stock['id']; ?>" class="bg-yellow-500 text-white px-2 py-1 rounded hover:bg-yellow-600" title="Edit">
+                                            <i class="glyphicon glyphicon-edit"></i>
+                                        </a>
+                                        <a href="delete_stock.php?id=<?php echo (int)$stock['id']; ?>" onClick="return confirm('Are you sure you want to delete?')" class="bg-red-500 text-white px-2 py-1 rounded hover:bg-red-600" title="Remove">
+                                            <i class="glyphicon glyphicon-trash"></i>
+                                        </a>
+                                    </div>
+                                </td>
+                            </tr>
                         <?php endforeach; ?>
-                        <!--     *************************     -->
                     </tbody>
                 </table>
-                <!--     *************************     -->
             </div>
         </div>
     </div>
-</div>
 
-<?php
-/**
-    print "<pre>";
-    print_r($all_stock);
-    print "</pre>\n";
-**/
-?>
+    <?php include_once('layouts/footer.php'); ?>
+</body>
 
-<?php include_once('layouts/footer.php'); ?>
+</html>
