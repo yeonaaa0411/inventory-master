59069fdfbf091a3528177ca6ea5b43e7f3fc01e0
yeonaaa0411 <marvinkristianmcruz2@gmail.com>
2024-11-06 07:49:04 +0800

    11/6/2024

--- bpsr_inv.sql
+++ bpsr_inv.sql
@@ -565,7 +565,7 @@ ALTER TABLE `sales`
 ALTER TABLE `users`
   ADD CONSTRAINT `FK_user` FOREIGN KEY (`user_level`) REFERENCES `user_groups` (`group_level`) ON DELETE CASCADE ON UPDATE CASCADE;
 <div class="error"><h1>Error</h1><p><strong>SQL query:</strong>
-<a href="db_sql.php?sql_query=SET+time_zone+%3D+%22SYSTEM%22&amp;show_query=1&amp;db=bpsr_inv"><span class="nowrap"><img src="themes/dot.gif" title="Edit" alt="Edit" class="icon ic_b_edit" />&nbsp;Edit</span></a>    </p>
+<a href="db_sql.php?sql_query=SET+time_zone+%3D+%22SYSTEM%22&amp;show_query=1&amp;db=sql12742917"><span class="nowrap"><img src="themes/dot.gif" title="Edit" alt="Edit" class="icon ic_b_edit" />&nbsp;Edit</span></a>    </p>
 <p>
 <code class="sql"><pre>
 SET time_zone = &quot;SYSTEM&quot;
--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_HOST', 'sql12.freemysqlhosting.net'); // Update with your actual host
-define('DB_USER', 'sql12742917');                // Your database username
-define('DB_PASS', 'YjQg1PGRWb');                  // Your database password
-define('DB_NAME', 'sql12742917');                 // Your database name
 
+  define( 'DB_HOST', 'localhost' );          // Set database host
+  define( 'DB_USER', 'root' );             // Set database user
+  define( 'DB_PASS', 'p1r4sp' );             // Set database password
+  define( 'DB_NAME', 'sql12742917' );        // Set database name
 
 ?>
