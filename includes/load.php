0ca7fa8100c0e6fd9f9aca37bca994a6c51e9f77
yeonaaa0411 <marvinkristianmcruz2@gmail.com>
2024-11-08 03:29:10 +0800

    4

--- includes/load.php
+++ includes/load.php
@@ -1,21 +1,22 @@
 <?php
-// load.php
-
-// Define path constants and include necessary files
+// -----------------------------------------------------------------------
+// DEFINE SEPERATOR ALIASES
+// -----------------------------------------------------------------------
 define("URL_SEPARATOR", '/');
 define("DS", DIRECTORY_SEPARATOR);
-defined('SITE_ROOT') ? null : define('SITE_ROOT', realpath(dirname(__FILE__)));
-define("LIB_PATH_INC", SITE_ROOT . DS);
 
-// Include necessary files in a clean order
-require_once(LIB_PATH_INC . 'config.php');
-require_once(LIB_PATH_INC . 'functions.php');
-require_once(LIB_PATH_INC . 'session.php');  // session.php starts the session
-require_once(LIB_PATH_INC . 'upload.php');
-require_once(LIB_PATH_INC . 'database.php');
-require_once(LIB_PATH_INC . 'sql.php');
-?>
+// -----------------------------------------------------------------------
+// DEFINE ROOT PATHS
+// -----------------------------------------------------------------------
+defined('SITE_ROOT')? null: define('SITE_ROOT', realpath(dirname(__FILE__)));
+define("LIB_PATH_INC", SITE_ROOT.DS);
 
+require_once(LIB_PATH_INC.'config.php');
+require_once(LIB_PATH_INC.'functions.php');
+require_once(LIB_PATH_INC.'session.php');
+require_once(LIB_PATH_INC.'upload.php');
+require_once(LIB_PATH_INC.'database.php');
+require_once(LIB_PATH_INC.'sql.php');
 
 // Start the session (make sure this is done before accessing any session variables)
 if (session_status() === PHP_SESSION_NONE) {
