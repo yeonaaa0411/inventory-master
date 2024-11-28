d9446c7ac2d28d42422ec7524d5151309748821d
Marvin Kristian M. Cruz <143852958+yeonaaa0411@users.noreply.github.com>
2024-11-29 03:50:27 +0800

    Update delete_order.php

--- delete_order.php
+++ delete_order.php
@@ -17,16 +17,9 @@
   $sales = find_sales_by_order_id($d_order['id']);
 
   // For each sale, delete it and increase the stock quantity
-  if (!empty($sales)) {
-    foreach ($sales as $sale) {
-      // Ensure 'quantity' and 'product_id' keys exist
-      if (isset($sale['quantity'], $sale['product_id'])) {
-        if (delete_by_id('sales', (int)$sale['id'])) {
-          increase_product_qty($sale['quantity'], $sale['product_id']);
-        }
-      } else {
-        error_log("Missing keys in sale record: " . print_r($sale, true));
-      }
+  foreach ($sales as $sale) {
+    if (delete_by_id('sales', (int)$sale['id'])) {
+      increase_product_qty($sale['quantity'], $sale['product_id']);
     }
   }
 
